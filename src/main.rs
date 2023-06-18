ca2ae51e2dfb6423e8f28dde8fd05e96741787e1
shominalexander <40673785+shominalexander@users.noreply.github.com>
2023-06-18 21:18:57 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,19 @@ fn main() {
    }//if current.iter().position(|&key| key == device_query::Keycode::LAlt ).is_some() {
 
    if current.iter().position(|&key| key == device_query::Keycode::RAlt ).is_some() {
-    scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D );
+    scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A );
 
    }//if current.iter().position(|&key| key == device_query::Keycode::RAlt ).is_some() {
+
+   if current.iter().position(|&key| key == device_query::Keycode::S ).is_some() {
+    scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D );
+
+   }//if current.iter().position(|&key| key == device_query::Keycode::S ).is_some() {
+
+   if current.iter().position(|&key| key == device_query::Keycode::W ).is_some() {
+    scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A );
+
+   }//if current.iter().position(|&key| key == device_query::Keycode::W ).is_some() {
 
    if current.iter().position(|&key| key == device_query::Keycode::Tab).is_some() {
     winput::send_inputs( [ winput::Input::from_vk( winput::Vk::LeftWin, winput::Action::Press   )
