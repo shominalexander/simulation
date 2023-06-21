7b0aa13578e71174dbf88754eedfe140f020a1b3
shominalexander <40673785+shominalexander@users.noreply.github.com>
2023-06-21 07:59:49 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -29,11 +29,10 @@ fn main() {
 
    previous = current.clone();
 
-   if current.iter().position(|&key| key == device_query::Keycode::Grave ).is_some() { scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); }
-   if current.iter().position(|&key| key == device_query::Keycode::LAlt  ).is_some() { scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); }
-   if current.iter().position(|&key| key == device_query::Keycode::RAlt  ).is_some() { scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); }
+   if current.iter().position(|&key| key == device_query::Keycode::Grave ).is_some() { scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); }
+   if current.iter().position(|&key| key == device_query::Keycode::LAlt  ).is_some() { scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); }
    if current.iter().position(|&key| key == device_query::Keycode::S     ).is_some() { scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); }
-   if current.iter().position(|&key| key == device_query::Keycode::Tab   ).is_some() { scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); }
+   if current.iter().position(|&key| key == device_query::Keycode::Tab   ).is_some() { scan_send( 0x1D ); sleep(duration); scan_send( 0x1D ); }
    if current.iter().position(|&key| key == device_query::Keycode::W     ).is_some() { scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); sleep(duration); scan_send( 0x2A ); }
 
    if current.iter().position(|&key| key == device_query::Keycode::CapsLock).is_some() {
