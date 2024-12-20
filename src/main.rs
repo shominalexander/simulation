c79b1f589778c028dc7b3b267660b9e5d2843090
shominalexander <40673785+shominalexander@users.noreply.github.com>
2024-12-20 11:09:14 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -39,22 +39,22 @@ fn main() {
 
    if current.iter().position(|&key| key == device_query::Keycode::Meta     ).is_some() { continue; } 
 
-   if current.iter().position(|&key| key == device_query::Keycode::J        ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 02, 0x0036 ); }
-   if current.iter().position(|&key| key == device_query::Keycode::K        ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 03, 0x0036 ); }
-   if current.iter().position(|&key| key == device_query::Keycode::L        ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 04, 0x0036 ); }
+   if current.iter().position(|&key| key == device_query::Keycode::LShift   ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 02, 0x0036 ); }
+   if current.iter().position(|&key| key == device_query::Keycode::Grave    ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 03, 0x0036 ); }
 
-   if current.iter().position(|&key| key == device_query::Keycode::LShift   ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 06, 0x0036 ); }
-   if current.iter().position(|&key| key == device_query::Keycode::Grave    ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 08, 0x0036 ); }
+   if current.iter().position(|&key| key == device_query::Keycode::J        ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 04, 0x0036 ); }
+   if current.iter().position(|&key| key == device_query::Keycode::K        ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 06, 0x0036 ); }
+   if current.iter().position(|&key| key == device_query::Keycode::L        ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 08, 0x0036 ); }
 
    if current.iter().position(|&key| key == device_query::Keycode::Home     ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 10, 0x0036 ); }
    if current.iter().position(|&key| key == device_query::Keycode::PageUp   ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_SCANCODE   , 0x00, 12, 0x0036 ); }
 
-   if current.iter().position(|&key| key == device_query::Keycode::M        ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 02, 0xE01D ); }
-   if current.iter().position(|&key| key == device_query::Keycode::Comma    ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 03, 0xE01D ); }
-   if current.iter().position(|&key| key == device_query::Keycode::Dot      ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 04, 0xE01D ); }
+   if current.iter().position(|&key| key == device_query::Keycode::LControl ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 02, 0xE01D ); }
+   if current.iter().position(|&key| key == device_query::Keycode::Tab      ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 03, 0xE01D ); }
 
-   if current.iter().position(|&key| key == device_query::Keycode::LControl ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 06, 0xE01D ); }
-   if current.iter().position(|&key| key == device_query::Keycode::Tab      ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 08, 0xE01D ); }
+   if current.iter().position(|&key| key == device_query::Keycode::M        ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 04, 0xE01D ); }
+   if current.iter().position(|&key| key == device_query::Keycode::Comma    ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 06, 0xE01D ); }
+   if current.iter().position(|&key| key == device_query::Keycode::Dot      ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 08, 0xE01D ); }
 
    if current.iter().position(|&key| key == device_query::Keycode::End      ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 10, 0xE01D ); }
    if current.iter().position(|&key| key == device_query::Keycode::PageDown ).is_some() { scan_send( winapi::um::winuser::KEYEVENTF_EXTENDEDKEY, 0xA3, 12, 0xE01D ); }
